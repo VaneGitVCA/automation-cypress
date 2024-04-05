2fdbad6e903283bb9ed17f8cadc96ed2325b02f9
VaneGitVCA <89807772+VaneGitVCA@users.noreply.github.com>
2024-04-05 17:36:23 -0600

    Reto completo
    Se realizan cambios en el proyecto en todos los it

--- cypress/e2e/front-end/register.cy.js
+++ cypress/e2e/front-end/register.cy.js
@@ -6,71 +6,58 @@ describe('Validaciones en regitrar', () => {
     cy.visit('/')
   })
 
-  // it('TC-009: FE | Filter by phones', () => {
-  //     cy.contains('Phones').click();
-  //     cy.get('#tbodyid').should('be.visible');
-  //     cy.screenshot();
-  // })
-  // it('TC-010: FE | Filter by laptops', () => {
-  //     cy.contains('Laptops').click();
-  //     cy.get('#tbodyid').should('be.visible');
-  //     cy.screenshot();
-  // })
-  // it('TC-011: FE | Filter by monitors', () => {
-  //     cy.contains('Monitors').click();
-  //     cy.get('#tbodyid').should('be.visible');
-  //     cy.screenshot();
-  // })
-  const randomuser = Math.random().toString(5).substring(2.2) + "vca"
+  const randomuser = Math.random().toString(36).substring(2.15) + "vca"
 
-  // it('TC-01: FE | Registrar un nuevo usuario', () => {     
-  //   cy.get('#username').type(randomuser)
-  //   cy.get('#firstName').type('Cruz')
-  //   cy.get('#lastName').type('Arce')
-  //   cy.get('#password').type ('!@#$123Vane')
-  //   cy.get('#confirmPassword').type('!@#$123Vane')
-  //   cy.get('.btn-default').click()
-  //   cy.get('.result').should('be.visible')
-  // });
+   it('TC-01: FE | Registrar un nuevo usuario', () => {     
+     cy.get('#username').type(randomuser)
+     cy.get('#firstName').type('Cruz')
+     cy.get('#lastName').type('Arce')
+     cy.get('#password').type ('!@#$123Vane')
+     cy.get('#confirmPassword').type('!@#$123Vane')
+     cy.get('.btn-default').click()
+     cy.get('.result').should('contain.text', 'Registration is successful');
+   });
+   
 
-  // it('TC-02 FE | Registrar un usuario con un password diferente', () => {
-  //   cy.get('#username').type(randomuser)
-  //   cy.get('#firstName').type('Cruz')
-  //   cy.get('#lastName').type('Arce')
-  //   cy.get('#password').type ('!@#$123Vane')
-  //   cy.get('#confirmPassword').type('!@#$123')
-  //   cy.get(':nth-child(5) > .alert').should('be.visible')
-  // });
+   it('TC-02 FE | Registrar un usuario con un password diferente', () => {
+     cy.get('#username').type(randomuser)
+     cy.get('#firstName').type('Cruz')
+     cy.get('#lastName').type('Arce')
+     cy.get('#password').type ('!@#$123Vane')
+     cy.get('#confirmPassword').type('!@#$123')
+     cy.get(':nth-child(5) > .alert').should('contain.text', 'Passwords do not match');
+   });
+   
 
-  //it('TC-03 FE | Registrar usuario que no cumpla con las politicas del password.', () => {
-  //  cy.get('#username').type(randomuser)
-  //  cy.get('#firstName').type('Cruz')
-  //  cy.get('#lastName').type('Arce')
-  //  cy.get('#password').type('!@#$123vane')
-  //  cy.get('#confirmPassword').type('!@#$123vane')
-  //  cy.get('.btn-default').click()
-  //  cy.get('.result').should('be.visible')
-  //});
+  it('TC-03 FE | Registrar usuario que no cumpla con las politicas del password.', () => {
+    cy.get('#username').type(randomuser)
+    cy.get('#firstName').type('Cruz')
+    cy.get('#lastName').type('Arce')
+    cy.get('#password').type('!@#$123vane')
+    cy.get('#confirmPassword').type('!@#$123vane')
+    cy.get('.btn-default').click()
+    cy.get('.result').should('contain.text', 'InvalidPasswordException: Password did not conform with policy: Password must have uppercase characters');
+  });
+  
 
-  //it('TC-04 FE | Ingresar un usuario ya registrado', () => {
-  //  cy.get('#username').type('VaneCA09')
-  //  cy.get('#firstName').type('Cruz')
-  //  cy.get('#lastName').type('Arce')
-  //  cy.get('#password').type('!@#$%12Va')
-  //  cy.get('#confirmPassword').type('!@#$%12Va')
-  //  cy.get('.btn-default').click() 
-  //  cy.get('.result').should('be.visible')
-  //});
+  it('TC-04 FE | Ingresar un usuario ya registrado', () => {
+    cy.get('#username').type('VaneCA09')
+    cy.get('#firstName').type('Cruz')
+    cy.get('#lastName').type('Arce')
+    cy.get('#password').type('!@#$%12Va')
+    cy.get('#confirmPassword').type('!@#$%12Va')
+    cy.get('.btn-default').click() 
+    cy.get('.result').should('contain.text', 'UsernameExistsException: User already exists');    
+  });
+  
 
-  it('TC-05 FE | Votor por un auto ', () => {
+  it('TC-05 FE | Validar voto ', () => {
     cy.visit('https://buggy.justtestit.org/')
     cy.get('.input-sm').type('VaneCA09')
     cy.get('[name="password"]').type('!@#$%12Va')
     cy.get('.btn-success').click()
     cy.get(':nth-child(3) > .card > a > .img-fluid').click()
-    cy.get(':nth-child(1) > .thumbnail > a > .img-thumbnail').click()
-    cy.get(':nth-child(3) > .card > a > .img-fluid').type('COMENTARIO')
-
+    cy.get(':nth-child(1) > .thumbnail > a > .img-thumbnail').click()   
+    cy.get('.card-text').should('contain.text', 'Thank you for your vote!');
   });
-
 });
